fec267ce530203dd681855904958594a128e922f
Łukasz Paczos <lukas.paczos@gmail.com>
2025-11-16 19:43:35 +0100

    add light.rgbw_color support

--- custom_components/ads_extended/light.py
+++ custom_components/ads_extended/light.py
@@ -5,10 +5,12 @@ from __future__ import annotations
 from typing import Any
 
 import pyads
+from pyads.constants import PLCTYPE_ARR_UINT
 import voluptuous as vol
 
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
+    ATTR_RGBW_COLOR,
     PLATFORM_SCHEMA as LIGHT_PLATFORM_SCHEMA,
     ColorMode,
     LightEntity,
@@ -24,13 +26,16 @@ from .entity import AdsEntity
 from .hub import AdsHub
 
 CONF_ADS_VAR_BRIGHTNESS = "adsvar_brightness"
+CONF_ADS_VAR_RGBW_COLOR = "adsvar_rgbw_color"
 STATE_KEY_BRIGHTNESS = "brightness"
+STATE_KEY_RGBW_COLOR = "rgbw_color"
 
 DEFAULT_NAME = "ADS Light"
 PLATFORM_SCHEMA = LIGHT_PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_ADS_VAR): cv.string,
         vol.Optional(CONF_ADS_VAR_BRIGHTNESS): cv.string,
+        vol.Optional(CONF_ADS_VAR_RGBW_COLOR): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     }
 )
@@ -47,10 +52,10 @@ def setup_platform(
 
     ads_var_enable: str = config[CONF_ADS_VAR]
     ads_var_brightness: str | None = config.get(CONF_ADS_VAR_BRIGHTNESS)
+    ads_var_rgbw_color: str | None = config.get(CONF_ADS_VAR_RGBW_COLOR)
     name: str = config[CONF_NAME]
 
-    add_entities([AdsLight(ads_hub, ads_var_enable, ads_var_brightness, name)])
-
+    add_entities([AdsLight(ads_hub, ads_var_enable, ads_var_brightness, ads_var_rgbw_color, name)])
 
 class AdsLight(AdsEntity, LightEntity):
     """Representation of ADS light."""
@@ -60,13 +65,19 @@ class AdsLight(AdsEntity, LightEntity):
         ads_hub: AdsHub,
         ads_var_enable: str,
         ads_var_brightness: str | None,
+        ads_var_rgbw_color: str | None,
         name: str,
     ) -> None:
         """Initialize AdsLight entity."""
         super().__init__(ads_hub, name, ads_var_enable)
         self._state_dict[STATE_KEY_BRIGHTNESS] = None
+        self._state_dict[STATE_KEY_RGBW_COLOR] = None
         self._ads_var_brightness = ads_var_brightness
-        if ads_var_brightness is not None:
+        self._ads_var_rgbw_color = ads_var_rgbw_color
+        if ads_var_rgbw_color is not None:
+            self._attr_color_mode = ColorMode.RGBW
+            self._attr_supported_color_modes = {ColorMode.RGBW}
+        elif ads_var_brightness is not None:
             self._attr_color_mode = ColorMode.BRIGHTNESS
             self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
         else:
@@ -84,10 +95,28 @@ class AdsLight(AdsEntity, LightEntity):
                 STATE_KEY_BRIGHTNESS,
             )
 
+        if self._ads_var_rgbw_color is not None:
+            await self.async_initialize_device(
+                self._ads_var_rgbw_color,
+                PLCTYPE_ARR_UINT(4),
+                STATE_KEY_RGBW_COLOR,
+            )
+
+
     @property
     def brightness(self) -> int | None:
         """Return the brightness of the light (0..255)."""
         return self._state_dict[STATE_KEY_BRIGHTNESS]
+
+    @property
+    def rgbw_color(self) -> tuple[int, int, int, int] | None:
+        """Return (R, G, B, W)."""
+        rgbw = self._state_dict[STATE_KEY_RGBW_COLOR]
+        if rgbw is None:
+            return None
+
+        # Convert ctype array to python tuple by accessing elements by index
+        return (int(rgbw[0]), int(rgbw[1]), int(rgbw[2]), int(rgbw[3]))
 
     @property
     def is_on(self) -> bool:
@@ -95,14 +124,27 @@ class AdsLight(AdsEntity, LightEntity):
         return self._state_dict[STATE_KEY_STATE]
 
     def turn_on(self, **kwargs: Any) -> None:
-        """Turn the light on or set a specific dimmer value."""
-        brightness = kwargs.get(ATTR_BRIGHTNESS)
+        """Turn the light on and set a specific dimmer or color value."""
         self._ads_hub.write_by_name(self._ads_var, True, pyads.PLCTYPE_BOOL)
 
+        brightness = kwargs.get(ATTR_BRIGHTNESS)
         if self._ads_var_brightness is not None and brightness is not None:
             self._ads_hub.write_by_name(
                 self._ads_var_brightness, brightness, pyads.PLCTYPE_UINT
             )
+
+        rgbw_color = kwargs.get(ATTR_RGBW_COLOR)
+        if self._ads_var_rgbw_color is not None and rgbw_color is not None:
+            if len(rgbw_color) == 4:
+                # Create c_uint16 array of length 4
+                arr_type = PLCTYPE_ARR_UINT(4)
+                arr = arr_type(*rgbw_color)
+
+                # Write to PLC
+                self._ads_hub.write_by_name(
+                    self._ads_var_rgbw_color, arr, arr_type
+                )
+
 
     def turn_off(self, **kwargs: Any) -> None:
         """Turn the light off."""
